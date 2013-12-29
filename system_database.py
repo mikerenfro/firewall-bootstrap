f8ff47ea3ff8957392e0420a63da2966b9643379
Mike Renfro <mike+github@renf.ro>
2013-12-29 11:28:29 -0600

    Starting to remove third zone

--- system_database.py
+++ system_database.py
@@ -2,24 +2,14 @@
 # and other tools.
 
 DEFAULT_DOMAIN = '__DOMAIN__'
-LOC_NETWORK = '__LOC_NETWORK__'
-LOC_NETWORK_PREFIX = '__LOC_NETWORK_PREFIX__'
-LOC_NETMASK_SHORT = '__LOC_NETMASK_SHORT__'
-DMZ_NETWORK = '__DMZ_NETWORK__'
-DMZ_NETWORK_PREFIX = '__DMZ_NETWORK_PREFIX__'
-DMZ_NETMASK_SHORT = '__DMZ_NETMASK_SHORT__'
+NETWORK = '__ETH1_NETWORK__'
+NETWORK_PREFIX = '__ETH1_NETWORK_PREFIX__'
+NETMASK_SHORT = '__ETH1_NETMASK_SHORT__'
 
-SYSTEM_DATABASE_LOC = [
+SYSTEM_DATABASE = [
     { 'host':    '__HOSTNAME__',
       'mac':     '__ETH1_MAC__',
       'ip':      '__ETH1_IP__',
-      },
-    ]
-
-SYSTEM_DATABASE_DMZ = [
-    { 'host':    '__HOSTNAME__-dmz',
-      'mac':     '__ETH2_MAC__',
-      'ip':      '__ETH2_IP__',
       'aliases': ('ns1',),
       },
     ]
